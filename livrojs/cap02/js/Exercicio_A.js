e777fa7f1e92b6723f7a6a148957bb2f3df38db7
Sterneves <sternevesneves@gmail.com>
2024-09-30 21:49:51 -0300

    Exercicio_A

---
+++ livrojs/cap02/js/Exercicio_A.js
@@ -0,0 +1,16 @@
+const frm = document.querySelector("form")
+const resp1 = document.querySelector("#outResp1")
+const resp2 = document.querySelector("#outResp2")
+const resp3 = document.querySelector("#outResp3")
+
+frm.addEventListener("submit",(e)=>{
+    const medicamento = frm.inMedicamento.value
+    const preco = Number(frm.inPreco.value)
+
+    const promocao = preco * 2
+    
+    resp1.innerText = `Medicamento: ${medicamento}`
+    resp2.innerText = `Promocao R$: ${promocao.toFixed.Fixed(5)}`
+    
+    e.preventDefalr()
+})
