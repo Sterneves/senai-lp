6fa36afdd6ed5f5633db3d875276a00d74723251
Sterneves <sternevesneves@gmail.com>
2024-10-01 21:43:00 -0300

    exee4_1

--- livrojs/cap04/js/exe4_1.js
+++ livrojs/cap04/js/exe4_1.js
@@ -1,4 +1,6 @@
 const frm = document.querySelector("form")
+const resp1 = document.querySelector("h3")
+const resp2 = document.querySelector("h4")
 
 frm.addEventListener("submit", (e)=>{
     e.preventDefault();
@@ -9,12 +11,18 @@ frm.addEventListener("submit", (e)=>{
 
     const media = (nota1 + nota2) / 2;
 
-    alert(media.toFixed(2));
+    //alert(media.toFixed(2));
+    resp1.innerText =`Media das notas: ${media.toFixed(2)}`;
 
     // se media>= 7 entãi "Aprovado" senão "Reprovado"
+    //alert(media >=7);
     if(media >= 7){
-        alert("Aprovado")
+       // alert("Aprovado")
+       resp2.innerText = `Parabens ${nome} aprovado!`;
+       resp2.style.color ="blue";
     }else{
-        alert("Reprovado")
+        //alert("Reprovado")
+        resp2.innerText= `Ops ${nome}! Você foi reprovado!`;
+        resp2.style.color ="red";
     }
 })
